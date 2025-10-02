917cf45264f9f863b88544304923a914133bf892
MANNEPALLI BALA PRAHARSHA <balapraharsha.m@gmail.com>
2025-10-02 21:46:46 +0530

    Update config.py

--- Python Executable Files/config.py
+++ Python Executable Files/config.py
@@ -1,5 +1,6 @@
 # Configuration file for paths, API keys, default folders
 BACKUP_PATH = "./Backups"
 DEFAULT_DOWNLOADS = "./Downloads"
-GEMINI_API_KEY = "AIzaSyBWHn8kXTzf7EE6cnYJMZuu5syug_RAMTk"
+GEMINI_API_KEY = "<YOUR_GOOGLE_API_KEY>"
 LOG_FILE = "./ai_system_log.txt"
+
